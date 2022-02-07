d92b5a553c7c3af08624c42e2948251df7774693
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-02-07 20:24:07 +0530

    updated

--- src/components/Events/Event.jsx
+++ src/components/Events/Event.jsx
@@ -82,12 +82,12 @@ const Event = ({ carousel, jury, card }) => {
                     <a href="https://filmfreeway.com/standalonefilmfestaward">
                         <Image src={"https://res.cloudinary.com/dykwfe4cr/image/upload/v1640528706/samples/rs_w_297_h_100_cg_true_es3bcm.jpg"} width={300} height={100} objectFit="contain" />
                     </a>
-                    <a href="#">
+                    {/* <a href="#">
                         <Image src={"https://res.cloudinary.com/dykwfe4cr/image/upload/v1640528722/samples/rs_w_398_h_100_cg_true_o6xbku.png"} width={300} height={100} objectFit="contain" />
                     </a>
                     <a href="#">
                         <Image src={"https://res.cloudinary.com/dykwfe4cr/image/upload/v1640528738/samples/rs_w_480_h_100_cg_true_pvuiq4.png"} width={300} height={100} objectFit="contain" />
-                    </a>
+                    </a> */}
                 </div>
             </div>
             <div className="event-rules">
@@ -98,20 +98,20 @@ const Event = ({ carousel, jury, card }) => {
                     <div className="event-rules-content-left" data-aos={`zoom-in`} >
                         <ul>
                             <li>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, quidem ducimus eveniet quam ullam repellat dicta delectus earum velit repudiandae fugit, doloribus, reprehenderit laborum vel fuga dolor sapiente. Incidunt, suscipit.</p>
+                                <p>SFFA created history. This is a unique kind of film festival for independent filmmakers/content creators where there are no barriers to borders, languages, genres, etc. Based in California, USA, this festival is having an online Award Nomination Festival, live screenings, and the Standalone film award festival in Los Angeles.</p>
                             </li>
                             <li>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, quidem ducimus eveniet quam ullam repellat dicta delectus earum velit repudiandae fugit, doloribus, reprehenderit laborum vel fuga dolor sapiente. Incidunt, suscipit.</p>
+                                <p>SFFA is consolidating filmmakers around the world for a wonderful presentation of cinematography and the unveiling of new talents. A committee comprising members from the film industry preview submitted films and select good works of motion pictures for the Award Nomination Festival.</p>
                             </li>
                         </ul>
                     </div>
                     <div className="event-rules-content-right" data-aos={`zoom-in-down`}>
                         <ul>
                             <li>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quae laudantium iusto voluptatem? Aut exercitationem doloribus quos. Beatae est facilis, quaerat cum sunt consectetur iusto aliquam illo quos soluta sint?</p>
+                                <p>The selected films participate in The Online Awards nomination Festival which is held between Jan-April. In the 2021 year, due to the pandemic situation, it was between February 1, and March 31, at our virtual theatre, available online across the World.</p>
                             </li>
                             <li>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quae laudantium iusto voluptatem? Aut exercitationem doloribus quos. Beatae est facilis, quaerat cum sunt consectetur iusto aliquam illo quos soluta sint?</p>
+                                <p>The selected films participate in The Online Awards nomination Festival which is held between Jan-April. In the 2021 year, due to the pandemic situation, it was between February 1, and March 31, at our virtual theatre, available online across the World.Films screen through this secure server screener where visitors and guests from around the world can witness and vote for the film. On the basis of viewers' choice and votes, films are nominated for 42 categories of Awards which screen live from the last week of May at Hollywood and to honor filmmakers, actors, technicians at The Standalone Awards Show in Los Angeles on the 7th day of live screenings.</p>
                             </li>
                         </ul>
                     </div>
@@ -121,21 +121,19 @@ const Event = ({ carousel, jury, card }) => {
                         <ul>
 
                             <li>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, quidem ducimus eveniet quam ullam repellat dicta delectus earum velit repudiandae fugit, doloribus, reprehenderit laborum vel fuga dolor sapiente. Incidunt, suscipit.</p>
+                                <p>We will screen all award-nominated films here as well as we are looking forward to creating year-round screening, filmmaking, and writing opportunities and to that end we are launching new festivals as part of the Standalone Film Festival & Awards.</p>
                             </li>
                             <li>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, quidem ducimus eveniet quam ullam repellat dicta delectus earum velit repudiandae fugit, doloribus, reprehenderit laborum vel fuga dolor sapiente. Incidunt, suscipit.</p>
+                                <p>In 2021, The film festival Award venue was the iconic TCL Chinese Theatre, in the year 2022, the Standalone film award festival in Los Angeles will again be in the heart of Hollywood.  </p>
                             </li>
                         </ul>
                     </div>
                     <div className="event-rules-content-right" data-aos={`zoom-in-down`}>
                         <ul>
                             <li>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quae laudantium iusto voluptatem? Aut exercitationem doloribus quos. Beatae est facilis, quaerat cum sunt consectetur iusto aliquam illo quos soluta sint?</p>
+                                <p>standalone awards Submissions are for feature narratives, Short films, Shows, Series, documentaries, and animation as even experimental films and music videos.</p>
                             </li>
-                            <li>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quae laudantium iusto voluptatem? Aut exercitationem doloribus quos. Beatae est facilis, quaerat cum sunt consectetur iusto aliquam illo quos soluta sint?</p>
-                            </li>
+                            
                         </ul>
                     </div>
                 </div>
