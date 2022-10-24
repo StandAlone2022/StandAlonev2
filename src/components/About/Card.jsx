3b8f4c91ab059d988e05b5d800e836dcb62c03d0
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-10-24 19:27:52 +0530

    removed unused code

--- src/components/About/Card.jsx
+++ src/components/About/Card.jsx
@@ -11,20 +11,6 @@ const Card = ({ img,name,role,about,id }) => {
                 <div className="card__back">
                     <div className="card__body">
                         <p>{about}</p>
-                        {/* <div className="links">
-                            <a href="#">
-                                <i className="fab fa-instagram" />
-                            </a>
-                            <a href="#">
-                                <i className="fab fa-facebook" />
-                            </a>
-                            <a href="#">
-                                <i className="fab fa-twitter" />
-                            </a>
-                            <a href="#">
-                                <i className="fab fa-pinterest" />
-                            </a>
-                        </div> */}
                     </div>
                 </div>
             </div>
